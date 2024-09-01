c5dd8b7cc81e4c4fcd9780ea77d30f7eee3b6972
muhamadzen669 <muhamadzen669@gmail.com>
2024-09-01 03:35:16 +0000

    table

--- src/logging/succesLog.ts
+++ src/logging/succesLog.ts
@@ -9,28 +9,15 @@ interface blockInfo {
 }
 
 export const succesLog = (blockInfo: blockInfo) => {
-  // const blockInfoParsed = {
-  //   index: blockInfo.index,
-  //   timestamp: blockInfo.timestamp,
-  //   hash: blockInfo.hash,
-  //   previousHash: blockInfo.previousHash,
-  //   signature: blockInfo.signature,
-  //   message: blockInfo.message,
-  //   nonce: blockInfo.nonce,
-  // }
+  const blockInfoParsed = {
+    index: blockInfo.index,
+    timestamp: blockInfo.timestamp,
+    hash: blockInfo.hash,
+    previousHash: blockInfo.previousHash,
+    signature: blockInfo.signature,
+    message: blockInfo.message,
+    nonce: blockInfo.nonce,
+  }
 
-  // console.table([blockInfoParsed])
-  // Contoh data dengan objek bersarang
-  const pegawai = [
-    { id: 1, nama: 'Alice', departemen: { nama: 'HR', lokasi: 'Jakarta' } },
-    { id: 2, nama: 'Bob', departemen: { nama: 'IT', lokasi: 'Bandung' } },
-    {
-      id: 3,
-      nama: 'Charlie',
-      departemen: { nama: 'Marketing', lokasi: 'Surabaya' },
-    },
-  ]
-
-  // Menampilkan data pegawai dengan detail departemen
-  console.table(pegawai, ['id', 'nama', 'departemen.nama'])
+  console.table([blockInfoParsed])
 }
