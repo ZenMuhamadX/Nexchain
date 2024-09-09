ab117dcf39ab3211839d0e0513e90b81d08bac31
muhamadzen669 <muhamadzen669@gmail.com>
2024-09-09 12:36:06 +0000

    ip

--- src/network/mainNet.ts
+++ src/network/mainNet.ts
@@ -1,8 +1,17 @@
 import { tcpNet } from './L1Net/tcp'
+import os from 'os'
+import 'dotenv/config'
 
 const PORT = process.env.PORT || 5000
 
+const ip = os.networkInterfaces().eth0!
+let ipAddress = ''
+if (ip[0].family === 'IPv4') {
+  ipAddress = ip[0].address
+}
+
 // Mulai server dan mendengarkan pada port
 tcpNet.listen(PORT, () => {
-  console.log(`network running ${PORT}`)
+  console.log(`network TCP running: ${ipAddress}:${PORT}`)
+  console.log(`network TCP running: localhost: ${PORT}`)
 })
