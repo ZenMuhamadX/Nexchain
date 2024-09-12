6d087c64a18e34c78a052c1388d84202bc4cb9f6
muhamadzen669 <muhamadzen669@gmail.com>
2024-09-12 11:31:11 +0000

    test

--- src/network/lib/handleComTcp.ts
+++ src/network/lib/handleComTcp.ts
@@ -6,6 +6,6 @@ const t = new BlockChains()
 const balance = t.getChains()[0].walletData
 export const processMessage = (message: string): any => {
 	if (message === 'balance') {
-		return balance
+		return balance.filter((item) => item.balance === 5000000)
 	}
 }
